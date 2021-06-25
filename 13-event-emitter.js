147dd41026cebec66c32424badc403514fef923d
Fardin Ahsan <fardin.ahsan75.com>
2021-06-25 16:28:39 +0600

    request event

--- 13-event-emitter.js
+++ 13-event-emitter.js
@@ -14,6 +14,7 @@ customEmitter.on('response', (name, id) => {
   console.log(`data recieved user ${name} with id:${id}`)
 })
 
+
 customEmitter.on('response', () => {
   console.log('some other logic here')
 })
